cc4b556e0901f1013b92c400924d288f674f7181
Alexande B <abobrikovich@gmail.com>
2020-04-02 20:04:26 +0300

    Fix tests for windows

--- test.sh
+++ test.sh
@@ -1,10 +1,21 @@
 #!/bin/env bash
 
-readonly __RFC2MDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # https://stackoverflow.com/a/246128/902217
-sed "s|\${BASEPATH}|${__RFC2MDDIR}|g" ${__RFC2MDDIR}/external/sgml_catalogue_files.xml.in > ${__RFC2MDDIR}/external/sgml_catalogue_files.xml
+case "$(uname -s)" in
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     	CATALOG_BASE=$(cmd //c "echo %cd:\\=/%")
+     	;;
 
-export XML_CATALOG_FILES="$PWD/external/sgml_catalogue_files.xml file://${PWD}/external/sgml_catalogue_files.xml"
-export SGML_CATALOG_FILES=${XML_CATALOG_FILES}
+   *)
+   		CATALOG_BASE="file://$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # https://stackoverflow.com/a/246128/902217
+     	;;
+esac
+
+CATALOG_BASE="${CATALOG_BASE}/external/"
+
+sed "s|\${BASEPATH}|${CATALOG_BASE}|g" ${PWD}/external/sgml_catalogue_files.xml.in > ${PWD}/external/sgml_catalogue_files.xml
+
+export XML_CATALOG_FILES="$PWD/external/sgml_catalogue_files.xml ${CATALOG_BASE}/sgml_catalogue_files.xml"
+export SGML_CATALOG_FILES="${XML_CATALOG_FILES}"
 export XML_DEBUG_CATALOG=1
 
 xsltproc --catalogs rfc2md.xslt rfcV2.xml
